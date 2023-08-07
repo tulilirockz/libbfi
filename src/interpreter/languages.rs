a044a9268ae8ce95d1133ca4c2a2350b8aa68f9a
tulili <tulilirockz.pub+git@gmail.com>
2023-08-07 01:34:22 -0300

    feat: add support for custom brainfuck dialects

--- src/interpreter/languages.rs
+++ src/interpreter/languages.rs
@@ -40,6 +40,16 @@ pub struct Ook;
 /// Blub. Blub? Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub! Blub? Blub? Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub? Blub! Blub! Blub? Blub! Blub? Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub. Blub! Blub. Blub. Blub. Blub! Blub.
 /// ```
 pub struct Blub;
+pub struct Custom {
+    pub add: char,
+    pub subtract: char,
+    pub left: char,
+    pub right: char,
+    pub print: char,
+    pub input: char,
+    pub jump_forwards: char,
+    pub jump_backwards: char,
+}
 
 impl Memory<Ook> {
     multi_char_tokens!(2, ".!?", "..", "!!", "?.", ".?", "!.", ".!", "!?", "?!");
@@ -52,6 +62,7 @@ impl Memory<Blub> {
 impl<Dialect> Memory<Dialect> {
     to_other_dialect!(ook, Ook);
     to_other_dialect!(blub, Blub);
+    to_other_dialect!(custom, Custom);
 }
 
 impl BFToken {
@@ -80,4 +91,109 @@ impl BFToken {
         String::from("Blub! Blub?"),
         String::from("Blub? Blub!")
     );
+    pub fn to_custom(&self, custom: &Custom) -> Result<char, TokenParseError> {
+        return Ok(match self {
+            BFToken::CellAdd => custom.add,
+            BFToken::CellSubtract => custom.subtract,
+            BFToken::PtrLeft => custom.left,
+            BFToken::PtrRight => custom.right,
+            BFToken::Print => custom.print,
+            BFToken::Input => custom.input,
+            BFToken::JumpForwards => custom.jump_forwards,
+            BFToken::JumpBackwards => custom.jump_backwards,
+            _ => return Err(TokenParseError),
+        });
+    }
+}
+
+impl Custom {
+    pub fn new(
+        add: char,
+        subtract: char,
+        left: char,
+        right: char,
+        print: char,
+        input: char,
+        jump_forwards: char,
+        jump_backwards: char,
+    ) -> Result<Self, String> {
+        let final_object = Self {
+            add,
+            subtract,
+            left,
+            right,
+            print,
+            input,
+            jump_forwards,
+            jump_backwards,
+        };
+        if final_object.has_duplicate_fields() {
+            return Err(String::from("Fields have duplicates"));
+        }
+        return Ok(final_object);
+    }
+
+    fn has_duplicate_fields(&self) -> bool {
+        let fields = [
+            &self.add,
+            &self.subtract,
+            &self.left,
+            &self.right,
+            &self.print,
+            &self.input,
+            &self.jump_forwards,
+            &self.jump_backwards,
+        ];
+
+        for (i, &outer_field) in fields.iter().enumerate() {
+            for &inner_field in fields.iter().skip(i + 1) {
+                if outer_field == inner_field {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+}
+
+impl Memory<Custom> {
+    pub fn add_tokens<T>(
+        &mut self,
+        custom: &Custom,
+        iterator: T,
+    ) -> Result<&mut Self, TokenParseError>
+    where
+        T: IntoIterator<Item = char>,
+    {
+        let mut thing: Vec<BFToken> = iterator
+            .into_iter()
+            .map(|token| {
+                return {
+                    if token == custom.add {
+                        BFToken::CellAdd
+                    } else if token == custom.subtract {
+                        BFToken::CellSubtract
+                    } else if token == custom.left {
+                        BFToken::PtrLeft
+                    } else if token == custom.right {
+                        BFToken::PtrRight
+                    } else if token == custom.print {
+                        BFToken::Print
+                    } else if token == custom.input {
+                        BFToken::Input
+                    } else if token == custom.jump_forwards {
+                        BFToken::JumpForwards
+                    } else if token == custom.jump_backwards {
+                        BFToken::JumpBackwards
+                    } else {
+                        BFToken::NoOP
+                    }
+                };
+            })
+            .filter(|x| *x != BFToken::NoOP)
+            .collect();
+        self.instruction_stack.append(&mut thing);
+        return Ok(self);
+    }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,11 +1,14 @@
 #[cfg(test)]
 use libbfi::interpreter::generic_dialect::Memory;
-use libbfi::{interpreter::languages::Ook, token::BFToken};
+use libbfi::{
+    interpreter::languages::{Custom, Ook},
+    token::BFToken,
+};
 
 #[test]
 fn test_adding_characters() {
     let mut binding = Memory::new();
-    let tokens = binding.add_tokens("+a-b>c<d[e]f".chars()).expect("amogus");
+    let tokens = binding.add_tokens("+a-b>c<d[e]f".chars()).unwrap();
 
     assert_eq!(
         tokens.instruction_stack,
@@ -26,9 +29,7 @@ fn test_to_ook() {
     let tokens: &mut Memory = bind.add_tokens("+a-b>c<d[e]f".chars()).expect("amogus");
 
     let mut ook: Memory<Ook> = tokens.to_ook();
-    let ooktoken: &mut Memory<Ook> = ook
-        .add_tokens::<Ook>("Ook. Ook. Ook. Ook.")
-        .expect("msFAILg");
+    let ooktoken: &mut Memory<Ook> = ook.add_tokens::<Ook>("Ook. Ook. Ook. Ook.").unwrap();
 
     assert_eq!(
         ooktoken.instruction_stack,
@@ -43,4 +44,40 @@ fn test_to_ook() {
             BFToken::CellAdd
         ]
     )
+}
+
+#[test]
+fn test_custom_type() {
+    let mytype = Custom {
+        add: 'p',
+        subtract: 'm',
+        left: 'l',
+        right: 'r',
+        print: 'x',
+        input: 'i',
+        jump_backwards: 'b',
+        jump_forwards: 'f',
+    };
+
+    let custom_program = "pmlrxifb";
+
+    let hi = Memory::new();
+    let mut program_runner = hi.to_custom();
+    program_runner
+        .add_tokens(&mytype, custom_program.chars())
+        .unwrap();
+
+    assert_eq!(
+        program_runner.instruction_stack,
+        vec![
+            BFToken::CellAdd,
+            BFToken::CellSubtract,
+            BFToken::PtrLeft,
+            BFToken::PtrRight,
+            BFToken::Print,
+            BFToken::Input,
+            BFToken::JumpForwards,
+            BFToken::JumpBackwards
+        ]
+    );
 }
