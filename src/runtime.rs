6387abec59052a283a67ffe8a4e992503b64d903
Orvar Segerström <orvarsegerstrom@gmail.com>
2024-05-01 12:20:12 +0200

    Improve doc comments

--- src/runtime.rs
+++ src/runtime.rs
@@ -7,8 +7,8 @@
 //! runtime.memory = env.memory;
 //! runtime.run_full_stack().clean_env()
 
-use std::{io::BufRead, io::Write};
 use crate::token::BFToken;
+use std::{io::BufRead, io::Write};
 
 pub trait Operator {
     fn op_ptr_left(&mut self);
@@ -22,9 +22,14 @@ pub trait Operator {
 }
 
 pub trait Runner: Clone + Sized {
+    /// Adds a sequence of tokens (instructions) to the runtime's instruction stack.
     fn add_tokens(&mut self, token_stream: Vec<BFToken>) -> &mut Self;
+    /// Resets the runtime environment to its initial state: instruction and pointer are set to zero,
+    /// memory is cleared (all cells set to 0), and the instruction stack is emptied.
     fn clean_env(&mut self) -> &mut Self;
+    /// Executes the next instruction from the instruction stack.
     fn next_instruction(&mut self, reader: &mut impl BufRead, writer: &mut impl Write)
         -> &mut Self;
+    /// Executes all instructions in the stack until the end is reached.
     fn run_full_stack(&mut self, reader: &mut impl BufRead, writer: &mut impl Write) -> &mut Self;
 }
